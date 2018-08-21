79c7b9939f01753895f8a220122629c5fcb9576e
Jason Bierque <bierquejason@gmail.com>
2018-08-22 01:17:29 +0200

    Create gameType.js

---
+++ SRC/Pyke/other/gameType.js
@@ -0,0 +1,10 @@
+module.exports = [{
+  "gameType": "CUSTOM_GAME",
+  "Description": "Custom games"
+},{
+  "gameType": "TUTORIAL_GAME",
+  "Description": "Tutorial games"
+},{
+  "gameType": "MATCHED_GAME",
+  "Description": "All other games"
+}]
