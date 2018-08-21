05e20c8d23651cb55d397a6b414e7ed3ddaf3d01
Jason Bierque <bierquejason@gmail.com>
2018-08-22 01:15:12 +0200

    Create gameMode.js

---
+++ SRC/Pyke/other/gameMode.js
@@ -0,0 +1,70 @@
+module.exports = [
+  {
+    "gameMode": "CLASSIC",
+    "Description": "Classic Summoner's Rift and Twisted Treeline games"
+  },
+  {
+    "gameMode": "ODIN",
+    "Description": "Dominion/Crystal Scar games"
+  },
+  {
+    "gameMode": "ARAM",
+    "Description": "ARAM games"
+  },
+  {
+    "gameMode": "TUTORIAL",
+    "Description": "Tutorial games"
+  },
+  {
+    "gameMode": "URF",
+    "Description": "URF games"
+  },
+  {
+    "gameMode": "DOOMBOTSTEEMO",
+    "Description": "Doom Bot games"
+  },
+  {
+    "gameMode": "ONEFORALL",
+    "Description": "One for All games"
+  },
+  {
+    "gameMode": "ASCENSION",
+    "Description": "Ascension games"
+  },
+  {
+    "gameMode": "FIRSTBLOOD",
+    "Description": "Snowdown Showdown games"
+  },
+  {
+    "gameMode": "KINGPORO",
+    "Description": "Legend of the Poro King games"
+  },
+  {
+    "gameMode": "SIEGE",
+    "Description": "Nexus Siege games"
+  },
+  {
+    "gameMode": "ASSASSINATE",
+    "Description": "Blood Hunt Assassin games"
+  },
+  {
+    "gameMode": "ARSR",
+    "Description": "All Random Summoner's Rift games"
+  },
+  {
+    "gameMode": "DARKSTAR",
+    "Description": "Dark Star: Singularity games"
+  },
+  {
+    "gameMode": "STARGUARDIAN",
+    "Description": "Star Guardian Invasion games"
+  },
+  {
+    "gameMode": "PROJECT",
+    "Description": "PROJECT: Hunters games"
+  },
+  {
+    "gameMode": "GAMEMODEX",
+    "Description": "Nexus Blitz games"
+  }
+]
