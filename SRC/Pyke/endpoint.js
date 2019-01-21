886aab4ffe4d905a0e89a7cb5283627d3fb0b294
Jason Bierque <bierquejason@gmail.com>
2019-01-21 11:08:03 +0100

    update to V4

--- SRC/Pyke/endpoint.js
+++ SRC/Pyke/endpoint.js
@@ -1,43 +1,43 @@
 module.exports = {
 
     champion_mastery:{
-        masteryById: '/lol/champion-mastery/v3/champion-masteries/by-summoner/',
+        masteryById: '/lol/champion-mastery/v4/champion-masteries/by-summoner/',
         masteryByIdByChampionId: '/by-champion/',
-        scoreById: '/lol/champion-mastery/v3/scores/by-summoner/'
+        scoreById: '/lol/champion-mastery/v4/scores/by-summoner/'
     },
     //Champion
     champion: {
-        championList: '/lol/platform/v3/champions',
-        championById: '/lol/platform/v3/champions/'
+        championList: '/lol/platform/v4/champions',
+        championById: '/lol/platform/v4/champions/'
     },
-    // Summoner api v3
+    // Summoner api v4
     summoner: {
-        summonerName: '/lol/summoner/v3/summoners/by-name/',
-        summonerId: '/lol/summoner/v3/summoners/',
-        accountId: '/lol/summoner/v3/summoners/by-account/'
+        summonerName: '/lol/summoner/v4/summoners/by-name/',
+        summonerId: '/lol/summoner/v4/summoners/',
+        accountId: '/lol/summoner/v4/summoners/by-account/'
     },
-    //League V3
+    //League V4
     league: {
-        bysummonerId: '/lol/league/v3/positions/by-summoner/',
-        masterleagues: '/lol/league/v3/masterleagues/by-queue/',
-        leagues: '/lol/league/v3/leagues/',
-        challengerleagues: '/lol/league/v3/challengerleagues/by-queue/'
+        bysummonerId: '/lol/league/v4/positions/by-summoner/',
+        masterleagues: '/lol/league/v4/masterleagues/by-queue/',
+        leagues: '/lol/league/v4/leagues/',
+        challengerleagues: '/lol/league/v4/challengerleagues/by-queue/'
     },
-    //LOL-STATUS-V3
+    //LOL-STATUS-V4
     status: {
-        shard_data: '/lol/status/v3/shard-data'
+        shard_data: '/lol/status/v4/shard-data'
     },
-    //MATCH-V3
+    //MATCH-V4
     match: {
-        matches: "/lol/match/v3/matches/",
-        matchlists: "/lol/match/v3/matchlists/by-account/",
-        timelines: '/lol/match/v3/timelines/by-match/',
-        tournament: '/lol/match/v3/matches/by-tournament-code/',
-        matchid_tournament: '/lol/match/v3/matches/'
+        matches: "/lol/match/v4/matches/",
+        matchlists: "/lol/match/v4/matchlists/by-account/",
+        timelines: '/lol/match/v4/timelines/by-match/',
+        tournament: '/lol/match/v4/matches/by-tournament-code/',
+        matchid_tournament: '/lol/match/v4/matches/'
     },
-    //SPECTATOR-V3
+    //SPECTATOR-V4
     spectator: {
-        activegames: '/lol/spectator/v3/active-games/by-summoner/',
-        feactured: '/lol/spectator/v3/featured-games'
+        activegames: '/lol/spectator/v4/active-games/by-summoner/',
+        feactured: '/lol/spectator/v4/featured-games'
     }
 }
