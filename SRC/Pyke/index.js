5f5d6640e5623e485ad7253bb1b174e7639f3718
Bierque Jason <bierquejason@gmail.com>
2019-02-05 15:41:54 +0100

    Update index.js

--- SRC/Pyke/index.js
+++ SRC/Pyke/index.js
@@ -1,9 +1,9 @@
-const championmasteriesv3 = require('./lib/CHAMPION-MASTERY-V3/Champion');
-const championv3 = require('./lib/CHAMPION-V3/champion');
-const summonerv3 = require('./lib/SUMMONER-V3/Summoner');
-const leaguev3 = require('./lib/LEAGUE-V3/league');
-const matchv3 = require('./lib/MATCH-V3/match');
-const spectatorv3 = require('./lib/SPECTATOR-V3/spectator');
+const championmasteriesv4 = require('./lib/CHAMPION-MASTERY/Champion');
+const championv4 = require('./lib/CHAMPION/champion');
+const summonerv4 = require('./lib/SUMMONER/Summoner');
+const leaguev4 = require('./lib/LEAGUE/league');
+const matchv4 = require('./lib/MATCH/match');
+const spectatorv4 = require('./lib/SPECTATOR/spectator');
 const apistatusv3 = require('./APIStatusAPI/index.js');
 
 const lib_ddragon = require('./lib_ddragon/lol-static-data');
@@ -11,14 +11,17 @@ const lib_ddragon = require('./lib_ddragon/lol-static-data');
 class Pyke {
     constructor(api_key) {
         this.api_key = api_key; // Your API_KEY https://developer.riotgames.com/
-        this.summoner = new summonerv3(this.api_key); // Summoner V3
-        this.masteries = new championmasteriesv3(this.api_key); // Masteries v3
-        this.champion = new championv3(this.api_key); // champion
-        this.league = new leaguev3(this.api_key); // League
-        this.match = new matchv3(this.api_key); // Match
-        this.spectator = new spectatorv3(this.api_key); // Ingame      
+        this.summoner = new summonerv4(this.api_key); // Summoner V3
+        this.masteries = new championmasteriesv4(this.api_key); // Masteries v3
+        this.champion = new championv4(this.api_key); // champion
+        this.league = new leaguev4(this.api_key); // League
+        this.match = new matchv4(this.api_key); // Match
+        this.spectator = new spectatorv4(this.api_key); // Ingame      
         //this.lol_static_data = console.log('The lol-static-data-v3 API is now deprecated and will be removed on Monday, August 27th, 2018. Please use Data Dragon as a replacement.');
-        this.discord = `https://discord.gg/riotapi`;
+        this.discord = {
+             "RiotAPI":'`https://discord.gg/riotapi',
+            "support_lib": "https://discord.gg/QgUnuk8"
+        }
         this.status = new apistatusv3(); //API Status V3
     }; 
 }
