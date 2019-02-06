bb1590c809d01218ba27ce246c88ab7564103339
Bierque Jason <bierquejason@gmail.com>
2019-02-06 15:32:45 +0100

    Update Exemple-ddragon.js

--- Exemple/Exemple-ddragon.js
+++ Exemple/Exemple-ddragon.js
@@ -2,4 +2,4 @@ const {DDragon, Pyke} = require('../main');
 
 const data = new DDragon();
 const pyke = new Pyke();
-
+pyke.summoner.getBySummonerName("DoctaEnkoda", "euw").then(console.log).catch(console.error);
